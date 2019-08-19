51caffd99c53648d77ec8f74837f47c6a519e0af
Andrew Jessen-Tyler <andrewtyl@gmail.com>
2019-08-19 16:44:59 -0500

    cipher and lotto implimented

--- app.js
+++ app.js
@@ -54,35 +54,93 @@ app.get('/cipher', (req, res) => {
             cipheredText = textToCipher;
         }
         else {
+
             let arr = [];
+
+
             for (let i = 0; i < textToCipher.length; i++) {
                 let currentCharCode = textToCipher.charCodeAt(i);
                 if ((currentCharCode >= 65) && (currentCharCode <= 90)) {
                     //character is uppercase
-                    if ((currentCharCode + shift) < 65){
-                        //character will loop around
-                        let tempCustomCharCode = (currentCharCode - 64); //1 = A, 2 = B, etc.
-                        tempCustomCharCode = (tempCustomCharCode + shift); //0 = Z, -1 = X, etc.
+                    let newCharCode = currentCharCode + shift;
+                    if (newCharCode  > 90) {
+                        newCharCode = newCharCode - 26;
                     }
-                    else {
-
-                    }
+                    arr.push(newCharCode);
                 }
                 if ((currentCharCode >= 97) && (currentCharCode <= 122)) {
                     //character is lowercase
+                    let newCharCode = currentCharCode + shift;
+                    if (newCharCode  > 122) {
+                        newCharCode = newCharCode - 26;
+                    }
+                    arr.push(newCharCode);
                 }
                 else {
                     //character is a special character
                     arr.push(currentCharCode);
                 }
             }
+
+            for (let i = 0; i < arr.length; i++){
+                arr[i] = String.fromCharCode(arr[i]);
+            }
+
             cipheredText = arr.join('');
         }
-        
+
         res.send(cipheredText);
     }
 })
 
 app.get('/lotto', (req, res) => {
-    res.send('Nothing implimented into the /lotto route yet.')
+    //Set Vars
+    let inputedLottoNums = req.param('arr');
+    let yourLottoNums = [];
+    let output = '';
+    let numCorrect = 0;
+
+    if (inputedLottoNums.length != 6) {
+        output = `Please submit 6 numbers in arr. IE: ?arr=1&arr=2&arr=3&arr=4&arr=5&arr=6`;
+    }
+    else {
+        for (let i = 0; i < 6; i++) {
+            let currentLottoNum = inputedLottoNums[i];
+            currentLottoNum = parseInt(currentLottoNum);
+            currentLottoNum = Math.floor(currentLottoNum);
+            if ((currentLottoNum == NaN) || (currentLottoNum < 0) || (currentLottoNum > 20)) {
+                output = 'Please make sure all values are numbers betweeo 1 and 20.';
+            }
+            else {
+                yourLottoNums.push(currentLottoNum);
+            }
+        }
+        if (output === '') {
+            for (let i = 0; i < 6; i++) {
+                let currentDrawnLottoNum = ((Math.random() * (21 - 1)) + 1);
+                currentDrawnLottoNum = Math.floor(currentDrawnLottoNum);
+                if (currentDrawnLottoNum === yourLottoNums[i]) {
+                    numCorrect++;
+                }
+            }
+
+            if (numCorrect < 4) {
+                output = "Sorry, you lose";
+            }
+            else if (numCorrect === 4) {
+                output = "Congratulations, you win a free ticket";
+            }
+            else if (numCorrect === 5) {
+                output = "Congratulations! you win $100";
+            }
+            else if (numCorrect === 6) {
+                output = "Wow! Unbelievable! you could have won the mega millions!";
+            }
+            else {
+                output = 'The lottery machine broke. See a technician.';
+            }
+        }
+    }
+
+    res.send(output);
 })
